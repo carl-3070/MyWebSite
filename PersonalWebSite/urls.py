dbe8bc051a693957f6f9669fd080339e34ca7b46
xiequanquan <quan.q.xie@gmail.com>
2019-03-18 17:28:30 +0800

    修改主页url

--- PersonalWebSite/urls.py
+++ PersonalWebSite/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
     path('mdeditor/', include('mdeditor.urls')),
 ]
